f60453df32ce710d6cb1c747634cedb9d48d6452
hari0082 <2430680@rts-365.dk>
2023-02-13 11:16:15 +0100

    add hero

--- js/index.js
+++ js/index.js
@@ -1,8 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+
+    document.querySelector('.hero').innerHTML = `
+    <figure class="hero-pic">
+    <img src="${hero.image}" alt="">
+    </figure>
+    <section class="hero-container">
+    <h1>${hero.headline}</h1>
+    <p>${hero.copy}</p>
+    <button class="hero-btn">
+    <img src="${hero.icon}" alt="">
+        <p>Explore</p>
+    </button>
+    </section>
+    `
+        
+
+
     // slå dig løs her... 
-
+    
     //eksempel på at udskrive alle overskrifter i services i konsollen:
-    services.forEach(service => console.log(service.headline))
-
+/* services.forEach(service => { 
+        console.log(service.text)
+}) */
+    
 }) // DOMContentLoaded slut
